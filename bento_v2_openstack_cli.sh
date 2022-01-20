f088a8febddbab449d722fb9bf151d09b1430ae3
P-O Quirion <pioliqui@gmail.com>
2022-01-20 17:06:03 -0500

    latest template for bento v2

--- bento_v2_openstack_cli.sh
+++ bento_v2_openstack_cli.sh
@@ -1,26 +1,77 @@
-PROJECT_NAME=ichange
-DATA_VOLUME_SIZE=5000 # 5TB
-FLAVOR=p4-6gb
-IMAGE=CentOS-7-x64-2020-11
+#!/bin/bash
+
+# DO that in options?
+PROJECT_NAME=bqc19
+DATA_VOLUME_SIZE=3000 # 3TB
+FLAVOR=p8-12gb
+IMAGE=Rocky-8-x64-2021-06
 LAN_ID=9ddbf1b2-a335-470b-a5b3-d3e4bd2b91cc
 
-openstack volume create --size ${DATA_VOLUME_SIZE} ${PROJECT_NAME}-data
-openstack volume create --size 20 ${PROJECT_NAME}-docker-sandbox
-openstack server create --key-name "poq sur moins" --flavor  ${FLAVOR} --image ${IMAGE} \
+DOCKER_VOLUME_SIZE=20
+ROOT_VOLUME_SIZE=10
+
+
+usage (){
+echo "usage: $0 Deploy a bento node with root, data and docker volumes"
+echo "options"
+echo "    -n        DO not create the volume to mount"
+
+}
+while getopts ":hn" opt; do
+  case $opt in
+  n)
+        NO_VOLUME=True
+        ;;
+  h)
+      usage
+      exit 0
+      ;;
+   \?)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+mkdir -p .tmp/
+YAML_CONFIG=.tmp/bento_v2_${PROJECT_NAME}.yaml
+cp template_bento_v2.yaml $YAML_CONFIG
+# BENTO_HOSTNAME=${PROJECT_NAME} envsubst < template_bento_v2.yaml \
+    # > $YAML_CONFIG
+
+# TODO check if the volume exist already instead of a having a flag
+ if [ -z ${NO_VOLUME+x} ]; then
+  openstack volume create --size ${DATA_VOLUME_SIZE} ${PROJECT_NAME}-data
+  openstack volume create --size 20 ${PROJECT_NAME}-docker-sandbox
+fi
+
+openstack server create --key-name "poq sur moins" --flavor  ${FLAVOR} \
+  --image ${IMAGE} \
   --nic net-id=${LAN_ID} --security-group default  \
-  --security-group bento-dev  --boot-from-volume 8  \
-  --user-data bento_v2_${PROJECT_NAME}.yaml   \
+  --security-group bento-dev  --boot-from-volume ${ROOT_VOLUME_SIZE}  \
+  --user-data  ${YAML_CONFIG}  \
   --block-device-mapping vdb=${PROJECT_NAME}-data  \
   --block-device-mapping vdc=${PROJECT_NAME}-docker-sandbox \
   ${PROJECT_NAME}
 
-SERVER_ID=$(openstack server list -f json |  jq -r   '.[] | select((."Name"=="ichange")).ID')
+SERVER_ID=$(openstack server list -f json \
+    |  jq -r   '.[] | select((."Name"=="'${PROJECT_NAME}'")).ID')
 
-ip=$(openstack  floating ip list   -f json | jq -r '.[] | select(."Fixed IP Address"==null)."Floating IP Address"')
+# get one floating ip
+ip=$(openstack  floating ip list   -f json \
+    | jq -r '[.[] | select(."Fixed IP Address"==null)][0]."Floating IP Address"')
 
+# create one if it os empty
 if [ -z "${ip}" ]; then
   openstack  floating ip create external-network
-  ip=$(openstack  floating ip list   -f json | jq -r '.[] | select(."Fixed IP Address"==null)."Floating IP Address"')
+  ip=$(openstack  floating ip list   -f json \
+    | jq -r '.[] | select(."Fixed IP Address"==null)."Floating IP Address"')
 fi
+
+while ! openstack server show  -f json ichange | jq -r '.status ' | grep 'ACTIVE'
+do
+  echo waiting for build to complete
+  sleep 2
+done
 
 openstack server add floating ip  ${PROJECT_NAME} ${ip}
