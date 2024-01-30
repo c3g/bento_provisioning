d5cae5d546c6fc8df83119b508b97ca63b1293ad
v-rocheleau <victor.rocheleau@mcgill.ca>
2024-01-30 17:59:01 -0500

    chore: add missing deps for python build

---
+++ configs/rocky_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Load base config
+source configs/.env
+
+# Instance specific configs
+PROJECT_NAME=bento-rocky-test                                   # Name of the project/instance
+DATA_VOLUME_SIZE=5                           # /data volume size in GB
+APP_VOLUME_SIZE=5                             # /app volume size in GB
+FLAVOR="ha4-15gb"                               # Openstack flavor for compute resources
+IMAGE="f95b59a2-99fd-4b7f-912c-d7f17640a791"    # Rocky 9,3
