d9659f6fa642cccc66bd1efccac94d43dabc4800
v-rocheleau <victor.rocheleau@mcgill.ca>
2024-01-30 14:31:47 -0500

    chore: runcmd installs required python version

--- configs/qa_config.sh
+++ configs/qa_config.sh
@@ -6,6 +6,6 @@ source configs/.env
 # Instance specific configs
 PROJECT_NAME="bento-qa"
 DATA_VOLUME_SIZE=1000
-APP_VOLUME_SIZE=200             # /data volume size in GB
+APP_VOLUME_SIZE=200
 FLAVOR="ha4-15gb"
 IMAGE="f95b59a2-99fd-4b7f-912c-d7f17640a791"    # Rocky 9,3
