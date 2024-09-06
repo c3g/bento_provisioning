f41dba66c9cf03c1ff73f4107af81736e486f1a7
Victor Rocheleau <victor.rocheleau@mcgill.ca>
2024-09-06 12:37:20 -0400

    TEExplorer and GDI configs

---
+++ configs/TEExplorer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Load base config
+source configs/.env
+
+# Instance specific configs
+PROJECT_NAME=TEEXplorer                                   # Name of the project/instance
+DATA_VOLUME_SIZE=25                           # /data volume size in GB
+APP_VOLUME_SIZE=50                             # /app volume size in GB
+FLAVOR="ha16-60gb"                               # Openstack flavor for compute resources
+IMAGE=$ROCKY_9_3_IMAGE_UUID
